3ce2a26078245926d2d73a53a01fa05b5d6dcd00
Kuba Mrozowski <stasmrozowski@gmail.com>
2017-09-15 13:27:06 +0200

    zadanie 2.2

--- src/Cars.java
+++ src/Cars.java
@@ -3,11 +3,9 @@ class Cars {
         Car car1 = new Car("BMW", "M5", 2007, 2.6); //marka, model, rocznik, pojemność silnika
         Car car2 = new Car("Opel", "Vectra", 2004, 1.8 );
 
-        System.out.println("Samochód 1:");
-        System.out.println("Marka: " + car1.brand + ", model: " + car1.model +
-                ", rocznik: " + car1.year + ", pojemność: " + car1.engine);
-        System.out.println("Samochód 2:");
-        System.out.println("Marka: " + car2.brand + ", model: " + car2.model +
-                ", rocznik: " + car2.year + ", pojemność: " + car2.engine);
+        System.out.printf("Samochód 1:"+'\n');
+        System.out.printf("Marka: "+car1.brand+" model: "+car1.model+" rocznik: "+car1.year+", pojemność: "+car1.engine+'\n');
+        System.out.printf("Samochód 2:"+'\n');
+        System.out.printf("Marka: "+car2.brand+", model: "+ car2.model+", rocznik: "+car2.year+", pojemność: "+car2.engine);
     }
 }
