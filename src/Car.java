317189e8d30abf6141a52aab1cc0652b794baa8f
Kuba Mrozowski <stasmrozowski@gmail.com>
2017-09-15 12:23:34 +0200

    zadanie 2.1

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+class Car {
+    String brand;
+    String model;
+    Integer year;
+    double engine;
+
+    Car(String b, String m, Integer y, double e){
+        brand = b;
+        model = m;
+        year = y;
+        engine = e;
+
+    }
+}
